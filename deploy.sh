2a6926acd8e5c5ca0b0fdfd1886a5bef5555173b
Foxdaxian <945039036@qq.com>
2019-07-05 19:53:12 +0800

    测试

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,10 @@ if which hexo 2>/dev/null; then
   echo "hexo exists!"
 else
   echo "nope, no hexo installed."
-  npm i hexo-cli -g
+  npm i hexo-cli -D
 fi
 
-
-a=$(npm root -g)
-ls -l $a
-
 cd ./blog
-$a/bin/hexo generate
+hexo generate
 mv ./public blog
 ll ./blog
